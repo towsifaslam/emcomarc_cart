5ff6b99e2fb271eae122e47eda47758c4d4a7e36
Towsif aslam <83607057+towsifaslam@users.noreply.github.com>
2022-08-30 19:45:56 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+function increment() {
+  let count = document.getElementById("count");
+  if (count.value >= 5) {
+    count.value = 5;
+    alert("Only five product allow");
+  } else {
+    count.value++;
+  }
+}
+function decrement() {
+  let count = document.getElementById("count");
+  if (count.value <= 0) {
+    count.value = 0;
+    alert("are you pol naki");
+  } else {
+    count.value--;
+  }
+}
+
+function crement() {
+  let count = document.getElementById("count");
+  if (count.value >= 5) {
+    count.value = 5;
+    alert("Only five product allow");
+  } else {
+    count.value++;
+  }
+}
